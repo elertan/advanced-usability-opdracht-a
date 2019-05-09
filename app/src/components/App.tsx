f6d2326ee3ab25585a69a7a3f0867f1645acfc74
Dennis Kievits <denkievits@gmail.com>
2019-05-09 11:24:28 +0200

    Base

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Root} from "./styles";
+import {Route, Router} from "react-router";
+import { createBrowserHistory } from 'history'
+import IndexScreen from "../screens/Index";
+
+const history = createBrowserHistory();
 
 const App: React.FC = () => {
   return (
     <Root>
-
+      <Router history={history}>
+        <Route exact path="/" component={IndexScreen} />
+      </Router>
     </Root>
   );
-}
+};
 
 export default App;
---
+++ app/src/screens/Index/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {Root} from "./styles";
+
+const IndexScreen: React.FC = () => {
+  return (
+    <Root>
+      index
+    </Root>
+  )
+};
+
+export default IndexScreen;
