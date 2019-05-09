735d081ce4e98d52ecc6f42dbb47b7ba150e4d9c
Dennis Kievits <denkievits@gmail.com>
2019-05-09 11:07:21 +0200

    Updates

---
+++ precache-manifest.89a03c1f2a9e5c6d9fff69f6df610856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472bb8d8fb8e1f601c98baecb90120a3",
+    "url": "/advanced-usability-opdracht-a/index.html"
+  },
+  {
+    "revision": "36cddaeb4ce13d4855a5",
+    "url": "/advanced-usability-opdracht-a/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/advanced-usability-opdracht-a/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "36cddaeb4ce13d4855a5",
+    "url": "/advanced-usability-opdracht-a/static/js/main.334ca79c.chunk.js"
+  },
+  {
+    "revision": "b4f345e1345ac36b7500",
+    "url": "/advanced-usability-opdracht-a/static/js/runtime~main.84098b79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/advanced-usability-opdracht-a/static/media/logo.5d5d9eef.svg"
+  }
+]);
