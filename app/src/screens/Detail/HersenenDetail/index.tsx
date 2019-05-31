2a1d0ad9bc43ebb1179baad6bb8905b7d47379f7
Robrecht Cornelis <robrechtcornelis7@hotmail.com>
2019-06-01 00:25:13 +0200

    Toelichting plaatjes, zelfde styling als Stem

--- app/src/screens/Detail/HersenenDetail/index.tsx
+++ app/src/screens/Detail/HersenenDetail/index.tsx
@@ -26,7 +26,7 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         <p>
           Tegenwoordig is het zelfs mogelijk om zonder lichamelijk contact een elektrisch apparaat, rolstoel of prothese te bedienen.
           Met alleen een functionerend brein tot uw beschikking, kunt u bijvoorbeeld een computer een programma laten opstarten en tekst op het beeld krijgen.
-          Of wat dacht u van het bedienen van uw rolstoel of prothese door er alleen maar goed over na te denken welke kant u op wilt?
+          Of wat dacht u van het bedienen van uw rolstoel of prothese, door alleen maar goed na te denken over welke richting u op wilt?
         </p>
 
         <h2>Waarom?</h2>
@@ -41,47 +41,48 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         </p>
 
         <p>
-         Deze technologie is nog erg jong en bevindt zich in de experimentfase.
+         Deze technologie is nog erg jong en bevindt zich in de experimentfase [4].
          De vele onderzoeken die op dit gebied worden gedaan zijn meer bedoeld om theorieën en concepten te bewijzen.
-         De technologie is daarmee dus nog lang niet klaar voor het brede publiek [4].
+         De technologie is daarmee dus nog lang niet klaar voor het brede publiek [3].
         </p>
 
         <h2>Hoe werkt de achterliggende techniek?	</h2>
 
         <p>
-          Alles wat zojuist genoemd is, wordt mogelijk gemaakt door een zogenaamd brein-computer aansluitingspunt.
-          Nee, dat betekent niet dat er een gaatje in uw hoofd moet worden geboord waar daarna een stekker in gaat.
+          Alles wat zojuist genoemd is, wordt mogelijk gemaakt door een zogenaamd brein-computer aansluitingspunt (BCA).
+          Nee, dat betekent niet dat er een gaatje in uw hoofd moet worden geboord waar daarna een kabel in gaat dat naar een apparaat leidt.
           Wat wordt er dan mee bedoeld?
         </p>
 
         <p>
-          Met een brein-computer aansluitingspunt wordt er bedoeld dat er een samenwerking is tussen een brein en een elektrisch apparaat [3].
+          Een BCA betekent het bestaan van een samenwerking tussen iemands brein en een elektrisch apparaat [3].
           Door deze samenwerking kan het apparaat signalen ontvangen die afkomstig zijn vanuit het brein.
           Deze signalen kunnen worden omgezet naar instructies voor het apparaat.
-          Hierdoor is het bijvoorbeeld mogelijk om tekst te schrijven op een beeldscherm of om een prothetisch ledemaat te bewegen door er alleen maar bewust over na te denken.
+          Hierdoor is het bijvoorbeeld mogelijk om tekst te schrijven op een beeldscherm, of om een prothetisch ledemaat te bewegen door er alleen maar bewust over na te denken.
           Door het omzetten van de signalen uit het brein naar elektrische golven, kan het apparaat de wensen van de gebruiker interpreteren en de juiste actie uitvoeren.
         </p>
 
         <p>	
           Maar goed, hoe krijgen we precies zo’n verbinding tussen brein en apparaat tot stand? 
-          Daar zijn twee manieren voor: tijdelijk een uitwendige kap/pet op het hoofd gebruiken tijdens de interactie met het apparaat (zie rechts), 
+          Daar zijn over het algemeen twee manieren voor [6, p.52]: tijdelijk een uitwendige kap/pet op het hoofd gebruiken tijdens de interactie met het apparaat (zie rechts), 
           of vrijwel permanent een inwendig stukje computerapparatuur in het hoofd brengen dat direct de hersenactiviteit kan meten. 
         </p>
 
         <img src={bcaVoorbeeld} />
+        <p><i>Een proefpersoon met uitwendige kap beweegt een prothese met zijn gedachten.</i></p>
         
         <p>
-          Ja, dat laatste leest u goed. Het is mogelijk een computerchip in het brein te plaatsen om zo een (draadloze) verbinding te maken met een elektrisch apparaat, en deze te bedienen met de kracht van uw hersenen. 
+          Ja, dat laatste leest u goed. Het is mogelijk een computerchip in het brein te plaatsen om zo een (draadloze) verbinding te maken met een elektrisch apparaat, en deze te bedienen met de kracht van uw hersenen. [31]
         </p>
 
         <p>
-          De toekomst is dichterbij dan u denkt, en is misschien zelfs al hier.
+          U kunt u afvragen wat nog <i>science fiction</i> is en wat de werkelijkheid...
         </p>
 
         <h2>Voor welke toepassingen wordt het gebruikt?</h2>
 
         <p>
-          We hebben het al vlug over een aantal toepassingen gehad. De mogelijkheden van brein-computer aansluitpunten kunnen op verschillende manieren een groot effect hebben op de levenskwaliteit van mensen.
+          We hebben het al vlug over een aantal toepassingen gehad. De mogelijkheden van BCAs kunnen op verschillende manieren een groot effect hebben op de levenskwaliteit van mensen [8][9].
           Stel dat iemand door een ongeluk of ziekte zijn of haar spreekvaardigheid of mobiliteit verliest. 
           Sommige mensen zijn compleet verlamd en kunnen niet eens meer hun ogen bewegen, laat staan hun armen, benen of handen.
         </p>
@@ -89,28 +90,38 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         <h3>Communicatie</h3>
 
         <p>
-          Brein-computer aansluitpunten kunnen gehandicapte personen helpen om hier minder hinder van te ervaren.
+          Brein-computer aansluitpunten kunnen gehandicapte personen helpen om hier minder hinder van te ervaren [9].
           De technologie kan een verlamd persoon in staat stellen om te communiceren met de buitenwereld, en zelfs weer hierin fysiek te laten bewegen.
-          In juni 2006 demonstreerde de Amerikaanse wetenschapper Peter Brunner de mogelijkheden van de technologie door voor het aanwezige publiek een boodschap op het scherm te weergeven,
-          waarin hij mededeelde dat hij niet zou kunnen werken zonder deze technologie [3]. Peter Brunner heeft ALS [5] en is compleet verlamd.
+          In juni 2006 demonstreerde de Amerikaanse wetenschapper Peter Brunner de mogelijkheden van de technologie, door voor het aanwezige publiek een boodschap op het scherm te weergeven,
+          waarin hij mededeelde dat hij niet zou kunnen werken zonder deze technologie [3]. Peter Brunner heeft ALS [5] en is compleet verlamd. De letters en resulterende woorden kwamen uit zijn gedachten
+          en werden door de computer opgevangen. Deze computer "typte" de woorden daarna op het beeldscherm. 
         </p>
 
         <img src={bcaChatVerlamming} />
+        <p><i>Twee verlamde mensen gebruiken hun gedachten om met elkaar te communiceren. Zij bedenken en sturen berichtjes naar elkaar via een computerprogramma.</i></p>
         
         <h3>Mobiliteit</h3>
 
         <p>
-          Zoals gezegd kan de technologie ook wonderen verrichten op het gebied van mobiliteitsproblemen. Een compleet verlamd persoon kan niet lopen, en een rolstoel is ook geen optie.
+          Zoals gezegd kan de technologie ook wonderen verrichten op het gebied van mobiliteitsproblemen [3][4][6][8][9]. Een compleet verlamd persoon kan niet lopen, en een rolstoel is ook geen optie.
           Zelfs een scootmobiel is geen alternatief aangezien deze op zijn minst bestuurd en aangedreven moet worden met de hand. 
-          Brein-computer aansluitpunten hebben de potentie om deze problemen op te lossen door het hulpstuk, in dit geval de scootmobiel of een prothese, te integreren met de persoon.
+          Brein-computer aansluitpunten hebben de potentie om deze problemen op te lossen door het hulpstuk, in dit geval de scootmobiel of een prothese, te integreren met de persoon [9].
           Of deze integratie nu via de inwendige of de uitwendige methode gebeurt, maakt voor het effect niet uit. 
           De integratie zou ervoor kunnen zorgen dat de persoon de scootmobiel met zijn of haar gedachten voorts kan sturen en draaien.
           Nog beter: in theorie is het mogelijk dat prothesen, zoals benen, armen, handen en voeten, net als echte ledematen kunnen functioneren.
-          Hiervoor wordt een verbinding gelegd tussen de hersenen en de prothesen. In dit geval is een scootmobiel helemaal niet meer nodig.
+          Hiervoor wordt een verbinding gelegd tussen de hersenen en de prothesen. In dit geval zou een scootmobiel helemaal niet meer nodig zijn.
         </p>
 
         <img src={bcaProthese1} />
         <img src={bcaProthese2} />
+        <p><i>Een proefpersoon beweegt zijn prothese met gedachten.</i></p>
+
+        <p>
+          Bij een verlamming kan het brein de spieren geen instructies meer geven om het lichaam te laten bewegen. 
+          Een brein-computer aansluitpunt kan dit wel doordat signalen direct aan de prothesen wordt doorgegeven via een elektrische ontvanger aan de prothese [9][32]. 
+          Simpel gezegd springen de wensen van de gebruiker direct vanuit het brein naar deze ontvanger. 
+          Op zijn beurt krijgt de prothese de juiste instructies van de ontvanger. Hierdoor zal de prothese de gewenste beweging maken.
+        </p>
 
         <h3>Waarnemen van de werkelijke emotionele status</h3>
 
@@ -130,7 +141,7 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         </p>
 
         <p>
-          Dat gezegd hebbende, zijn er wel studies gedaan naar de gebruikerservaring van verschillende brein-computer aansluitingspunten [6].
+          Dat gezegd hebbende, zijn er wel studies gedaan naar de gebruikerservaring van verschillende brein-computer aansluitingspunten [6][9].
           In een van deze studies is gekeken naar de volgende drie kenmerken van een BCA. Deze kenmerken behoren tot een standaarddefinitie (genaamd ISO) op het gebied van kwaliteit [7].
           Hier werd op gelet: prestatie, mentale belasting en voldoening, van de niet-gehandicapte testpersonen bij gebruik van de BCA.
           Tijdens de meting werden vragen gesteld als: hoe snel leert een persoon de technologie te gebruiken?
@@ -155,7 +166,7 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         <p>
           Vanwege de nog experimentele status van de beschreven technologie, zijn er weinig concrete toepassingen of integratie met apparaten die bij u thuis staan.
           U kunt niet zomaar een hoofdkapje kopen en deze aansluiten op uw computer of tablet, en deze fijn zonder handen besturen.
-          Er wordt wel veel onderzoek gedaan naar (het ontwikkelen van) commerciële toepassingen voor hedendaagse apparaten, en het is interessant om naar deze te kijken en te speculeren over de toekomst.
+          Er wordt wel veel onderzoek gedaan naar (het ontwikkelen van) commerciële toepassingen voor hedendaagse apparaten [9][11], en het is interessant om naar deze te kijken en te speculeren over de toekomst.
         </p>
 
         <h3>Computer, telefoon -of tabletbesturing</h3>
@@ -163,12 +174,14 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         <p>
           We hebben het er al eerder over gehad, maar we zouden geen muis en toetsenbord meer nodig moeten hebben met een BCA.
           Met onze gedachten kunnen we immers de computer besturen door het pijltje te bewegen naar de juiste icoontjes, of naar de letters om een bericht te schrijven.
-          Momenteel is het grootste nadeel, naast de experimentele status van BCA’s, dat er amper computerprogramma’s zijn buiten onderzoeksomgevingen die gemaakt zijn voor deze besturingswijze.
-          Er valt op dit gebied nog veel terrein te winnen. [4]. 
+          Momenteel is het grootste nadeel, naast de experimentele status van BCAs, dat er amper computerprogramma’s zijn buiten onderzoeksomgevingen die gemaakt zijn voor deze besturingswijze;
+          er valt op dit gebied nog veel terrein te winnen. [4]. 
         </p>
 
         <img src={bcaSchaakspel} />
         <img src={bcaCursorBesturing} />
+        
+        <p><i><b>Boven:</b> een man speelt een schaakspel op de computer met zijn gedachten. <b>Onder:</b> uitleg hoe een BCA te gebruiken om de pijl (cursor) over het computerbeeld naar het doelwit te bewegen.</i></p>
 
         <p>
           Aangezien telefoons en tablets ook kunnen worden gezien als computers, kunnen deze op dezelfde manier worden bestuurd.
@@ -179,7 +192,9 @@ const HersenenDetail: React.FC<IProps> = (props) => {
 
         <img src={bcaVrouw} />
         <img src={bcaAppScherm} />
-
+        
+        <p><i><b>Links:</b> een vrouw demonstreert de XWave (op het hoofd) en de app op de iPhone. <b>Rechts:</b> de app toont de huidige hersenactiviteit met meetwaarden.</i></p>
+        
         <h3>Meer en snellere adoptie van technologische hulpmiddelen</h3>
 
         <p>
@@ -189,13 +204,15 @@ const HersenenDetail: React.FC<IProps> = (props) => {
         </p>
 
         <p>
-          Op het gebied van prothesen kan de adoptiesnelheid vergroot en het leerproces versneld worden.
-          Doordat BCA’s van nature intuitief aanvoelen, zal het gebruik van een arm -of beenprothese sneller natuurlijk aanvoelen en aangeleerd worden.
-          Dit verlaagt de drempel tot het accepteren van een prothese als hulpstuk en maakt het gebruik ervan natuurlijker.
+          Op het gebied van prothesen zou de adoptiesnelheid vergroot en het leerproces versneld kunnen worden.
+          Doordat BCAs van nature intuitief aanvoelen, zal het gebruik van een arm -of beenprothese sneller natuurlijk aanvoelen en aangeleerd worden [9][33].
+          Dit verlaagt de drempel tot het accepteren van een prothese als hulpstuk en maakt het gebruik ervan natuurlijker [6].
         </p>
 
         <img src={bcaDiagram} />
         <img src={bcaRolstoel} />
+
+        <p><i><b>Links:</b> signalen worden gestuurd vanuit het brein en worden onderweg omgevormd tot een (digitale) vorm die te interpreteren is voor (medische) hulpmiddelen. <b>Rechts:</b> een rolstoelgebruiker met een XWave, waarmee hersensignalen via de iPhone naar de rolstoel kunnen.</i></p>
 
       </Content>
     </Root>
--- app/src/screens/Detail/HersenenDetail/styles.ts
+++ app/src/screens/Detail/HersenenDetail/styles.ts
@@ -9,7 +9,7 @@ export const Root = styled.div`
 `;
 
 export const Content = styled.div`
-  margin: 100px 100px;
+  margin: 100px 15%;  
   
   h1 {
     font-family: 'Playfair Display', serif;
@@ -27,5 +27,40 @@ export const Content = styled.div`
   
   p {
     margin: 10px 0;
+    display: block;
   }
+  ul {
+    margin: 15px 30px;
+    
+    li {
+      margin: 5px 0;
+    }
+  }
+  
+  img {
+    margin: 10px;
+    border-style: solid;
+    border-width: 1px;
+  }
+  #stap {
+    margin-left: 20px;
+    margin-right: 160px;
+  }
+  #google {
+    width: 200px;
+    height:400px;
+    margin-right: 10px;
+    
+  }
+  ul.bron {
+   list-style: none;
+   margin-left: 0;
+   padding-left: 1em;
+   text-indent: -2em;
+   li {
+   margin-bottom: 25px;
+    }
+  }
+  
+  
 `;
